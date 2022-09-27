de2ca7fd3018c4a741833716e00f959c753b2180
hojo <berna602@naver.com>
2022-09-27 22:18:02 +0900

    최신글 게시판 테스트 추가

--- theme/community/index.php
+++ theme/community/index.php
@@ -82,6 +82,16 @@ $(function() {
         ?>
     <div class="lt_more"><a href="<?php echo G5_BBS_URL ?>/new.php"><span class="sound_only">전체 게시판 최신글</span>더보기</a></div>
   </div>
+
+  <div class="lt_li">
+      <h2 class="lt_title"><a href="<?php echo G5_BBS_URL ?>/new.php">네에번째</a></h2>
+        <?php
+        // new_latest('스킨', '출력라인', '글자수', 'is_comment', cache_minute)
+        echo new_latest('theme/new_latest', 20, 20, false, 5);
+        ?>
+    <div class="lt_more"><a href="<?php echo G5_BBS_URL ?>/new.php"><span class="sound_only">전체 게시판 최신글</span>더보기</a></div>
+  </div>
+
 </div>
 
 	<div class="lt_li">
